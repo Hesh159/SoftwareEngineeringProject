4d76620d0628041b12166d8dc5390db256534adc
darragh <120389806@umail.ucc.ie>
2022-11-08 02:55:30 +0000

    Completed removeJunctionNeighbourPair method Completed unit tests for junctionNeighbourPair methods Initialized methods for adding and removing traffic lights

--- junction.py
+++ junction.py
@@ -8,10 +8,10 @@ class Junction():
 
 
     def __repr__(self):
-        return f"Id: {self._id}"
+        return f"Junction id: {self._id}"
 
     
-    #methods for creating and removing junction neighbour pairs
+    #methods for creating, removing, and viewing junction neighbour pairs
     #possibly add limit to number of Junctions in NeighbouringJunctions (logically cannot be more than 4)
     def addJunctionNeighbourPair(self, neighbouringJunctionToAdd: "Junction"):
         try:
@@ -22,6 +22,7 @@ class Junction():
                 self._neighbouringJunctions.append(neighbouringJunctionToAdd)
                 #calls the method again on the neighbouringJunctionToAdd object, using the current object as input
                 #a loop is avoided as once the neighbouringJunctionToAdd object calls the method again, the Junctions are in the list and this block of code is skipped.
+                print(f"Successfully added {neighbouringJunctionToAdd} to {self}")
                 neighbouringJunctionToAdd.addJunctionNeighbourPair(self)
 
         except TypeError:
@@ -29,42 +30,96 @@ class Junction():
         return
 
     def removeJunctionNeighbourPair(self, neighbouringJunctionToRemove: "Junction"):
-        pass
+        try:
+            if not isinstance(neighbouringJunctionToRemove, Junction):
+                raise TypeError
+
+            if neighbouringJunctionToRemove in self._neighbouringJunctions:  
+                self._neighbouringJunctions.remove(neighbouringJunctionToRemove)
+                print(f"Successfully removed {neighbouringJunctionToRemove} from {self}")
+                neighbouringJunctionToRemove._neighbouringJunctions.remove(self)
+
+        except TypeError:
+            print("TypeError: Input to removeJunctionNeighbourPair method must be an instance of Junction class")
+        return
 
     def getNeighbouringJunctions(self):
         return self._neighbouringJunctions
 
+    
+    #methods for adding and removing traffic lights to junctions trafficLightsInJunction list
+    def addTrafficLight(self, trafficLightToAdd):
+        pass
+
+    def removeTrafficLight(self, trafficLightToRemove):
+        pass
+
+    #get list of traffic lights in junction
+    def getTrafficLights(self):
+        return self._trafficLightsInJunction
+
 
 
 def testJunctionPairMethods():
+    print("Testing junctionNeighbourPair addition and removal methods...")
+    print("\n\n")
+
     #Attempt to run the addJunctionNeighbourPair method with invalid input, should raise TypeError
     print("Running addNeighbourPair() method with an input of 17...") 
-    J1.addJunctionNeighbourPair(17)
+    testJunction1 .addJunctionNeighbourPair(17)
 
-    #print J1s neighbouringJunctions (empty)
-    print(f"{J1} NeighbourList: {J1.getNeighbouringJunctions()}")
-
+    #print testJunction1 s neighbouringJunctions (empty)
+    print(f"{testJunction1 } NeighbourList: {testJunction1 .getNeighbouringJunctions()}")
+    print("\n")
 
     #Attempt to run the addJunctionNeighbourPair method successfully
-    print("Adding (J1, J2), (J1, J3), (J2, J4), (J3, J4) as neighbour pairs...")
-    J1.addJunctionNeighbourPair(J2)
-    J1.addJunctionNeighbourPair(J3)
-    J2.addJunctionNeighbourPair(J4)
-    J3.addJunctionNeighbourPair(J4)
+    print("Adding (testJunction1 ,  testJunction2), (testJunction1 , testJunction3),  testJunction2, testJunction4), (testJunction3, testJunction4) as neighbour pairs...")
+    testJunction1 .addJunctionNeighbourPair(testJunction2)
+    testJunction1 .addJunctionNeighbourPair(testJunction3)
+    testJunction2.addJunctionNeighbourPair(testJunction4)
+    testJunction3.addJunctionNeighbourPair(testJunction4)
+    print("\n")
 
     #print neighbouringJunctions of all Junctions
-    print(f"{J1} NeighbourList: {J1.getNeighbouringJunctions()}")
-    print(f"{J2} NeighbourList: {J2.getNeighbouringJunctions()}")
-    print(f"{J3} NeighbourList: {J3.getNeighbouringJunctions()}")
-    print(f"{J4} NeighbourList: {J4.getNeighbouringJunctions()}")
+    print("Printing neighbouringJunctions of all Junctions...")
+    print(f"{testJunction1 }, NeighbourList: {testJunction1 .getNeighbouringJunctions()}")  #2, 3
+    print(f"{testJunction2}, NeighbourList:   {testJunction2.getNeighbouringJunctions()}")  #1, 4
+    print(f"{testJunction3}, NeighbourList: {testJunction3.getNeighbouringJunctions()}")  #1, 4
+    print(f"{testJunction4}, NeighbourList: {testJunction4.getNeighbouringJunctions()}")  #2, 3
+    print("\n")
 
+
+    #test removeJunctionNeighbourPair with invalid input
+    print("Running removeJunctionNeighbourPair with an input of \"junction 2\"...")
+    testJunction1 .removeJunctionNeighbourPair("junction 2")    #TypeError
+    print(f"{testJunction1}, NeighbourList: {testJunction1 .getNeighbouringJunctions()}")
+    print("\n")
+
+    #test removeJunctionNeighbourPair with Junction not in neighbouringJunctions
+    print(f"Removing {testJunction1} and {testJunction4} neighbour pair...")
+    print(f"Before: {testJunction1}, NeighbourList: {testJunction1.getNeighbouringJunctions()}")
+    testJunction1.removeJunctionNeighbourPair(testJunction4)
+    print(f"After: {testJunction1}, NeighbourList: {testJunction1.getNeighbouringJunctions()}")
+    print("\n")
+
+    #test removeJunctionNeighbourPair successfully
+    print(f"Removing {testJunction1} and {testJunction2} neighbour pair...")
+    testJunction2.removeJunctionNeighbourPair(testJunction1)
+    print(f"{testJunction1}, NeighbourList: {testJunction1 .getNeighbouringJunctions()}")   #3
+    print(f"{testJunction2}, NeighbourList:   {testJunction2.getNeighbouringJunctions()}")  #4
+    print("\n")
+
+    print("Testing of junctionNeighbourPair methods complete")
+
+
+    
 
 
 if __name__ == "__main__":
-    J1= Junction(1)
-    J2 = Junction(2)
-    J3 = Junction(3)
-    J4 = Junction(4)
+    testJunction1 = Junction(1)
+    testJunction2 = Junction(2)
+    testJunction3 = Junction(3)
+    testJunction4 = Junction(4)
 
     print("Testing Junction addNeighbourPair and removeNeighbourPair methods...")
     testJunctionPairMethods()
