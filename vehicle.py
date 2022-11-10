4881367982dd8affb8b672aa124e6553f529f39e
darragh <120389806@umail.ucc.ie>
2022-11-10 12:54:28 +0000

    started Vehicle init method

--- vehicle.py
+++ vehicle.py
@@ -3,4 +3,12 @@ import random
 class vehicle():
 
     def __init__(self) -> None:
+        self._vehiceRoute = self.generateRoute()
+
+    def __repr__(self) -> str:
         pass
+
+
+    def generateRoute():
+        pass
+    
