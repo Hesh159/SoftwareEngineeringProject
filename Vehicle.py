259dad3a7b349b4390eb75d3bd1550887751623e
Samina Arshad <98948626+Kellziana@users.noreply.github.com>
2022-11-22 15:43:08 +0000

    Add files via upload

---
+++ Vehicle.py
@@ -0,0 +1,28 @@
+from random import randint
+import random
+import time
+
+class Vehicle():
+
+    def __init__(self) -> None:
+        self._dests = [1, 2, 3, 4]
+        self._generateVehicle = randint(1, 15)
+        self._behaviour = random(slow, fast)
+        slow = time(10)
+        fast = time(5)
+
+    
+def getBehaviour(self, slow, fast) -> str:
+    while True():
+        getBehaviour == slow | getBehaviour == fast
+        return self._behaviour
+
+
+def getGenerate(self) -> int:
+    while True():
+        if time.time == 10:
+           return self._generateVehicle
+
+if __name__ == "__main__":
+    testVehicle = Vehicle()
+    
