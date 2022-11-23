650beb39e7660b690904742c6ab47e2447bb83c1
darragh <120389806@umail.ucc.ie>
2022-11-23 13:18:51 +0000

    test_junction.py - created removeJunctionNeighourPair test methods

--- test_junction.py
+++ test_junction.py
@@ -23,8 +23,27 @@ class TestJunction(unittest.TestCase):
     def test_addJunctionNeighbourPairWrongInput(self):
         testjunction1 = Junction()
         WrongInput = 17
+
         self.assertRaises(TypeError, testjunction1.addJunctionNeighbourPair(WrongInput))
         
+
+    def test_removeJunctionNeighbourPair(self):
+        testjunction1 = Junction()
+        testjunction2 = Junction()
+        testjunction1.addJunctionNeighbourPair(testjunction2)
+        testjunction1.removeJunctionNeighbourPair(testjunction2)
+        expectedResultList = []
+        actualResultList = testjunction2.getNeighbouringJunctions()
+
+        self.assertEqual(actualResultList, expectedResultList)
+
+    def test_removeJunctionNeighbourPairBadInput(self):
+        testjunction1 = Junction()
+        testjunction1.addJunctionNeighbourPair()
+        badInput = "String"
+
+        self.assertRaises(TypeError, testjunction1.removeJunctionNeighbourPair(badInput))
+
 
 
 
