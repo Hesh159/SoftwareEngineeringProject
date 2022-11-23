91e322e820925d53b758cb9d6cfe4b2e823d9f7f
Klaudia <98896014+OkLuden@users.noreply.github.com>
2022-11-23 03:04:50 +0000

    Updated pedestrianLight.py
    I have added new variables and classes to pedestrianLight.
    The code was unclear to me so I would rather finish it tomorrow with assistance.

--- pedestrianLight.py
+++ pedestrianLight.py
@@ -1,14 +1,45 @@
 from light import Light
 from light import trafficLightStateChangeTests
+import time
 
 class PedestrianLight(Light):
 
+    trafficLightStates = ["Green", "Red", "Idle"]
+
     def __init__(self) -> None:
-        super().__init__()
+        super().__init__(trafficLightStatePointer, currentTrafficLightState) #Inherits
+        self._buttonPressed = False
+        self._blinkingLight = False
+        self._lightXRed = False #check if left light is red
+        self._lightYRed = False #check if right light is red
 
-    def __repr__(self) -> None:
-        super().__repr__()
+    '''def __repr__(self) -> None:
+        super().__repr__()'''           #I am unsure about the ID since it will be different to light.py, 
+                                        #no junctions either since this light is not directional
 
+    def button(self):
+        if '''read input''':
+            self._buttonPressed = True
+        else:
+            self._buttonPressed = False
+
+    def buttonPress(self):
+        if self._buttonPressed == True:
+            #alert other lights that they should go red
+
+    def checkLights(self):
+        if (self._lightXRed == True) & (self._lightYRed == True):
+            self._turnGreen()
+
+    def turnGreen(self):
+        time.sleep(5) #Wait 5 seconds before turning green
+        #change light state to 1 green
+        time.sleep(20)
+        self._blinkingLight = True #This is for GUI class
+        time.sleep(10) #10 seconds of blinking
+        #change light state to 2 red
+        self._blinkingLight = False
+        self._buttonPressed = False #Reset variables
 
     def changeTrafficLightState(self) -> None:
         return super().changeTrafficLightState(addAmount=2)
