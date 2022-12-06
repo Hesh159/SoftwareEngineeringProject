4929e3057dd38e74bb8ba42f574ba28845642cd8
darragh <120389806@umail.ucc.ie>
2022-12-06 01:14:34 +0000

    Light.py - created maxCarsAtLight getter and setter

--- TrafficLightController.py
+++ TrafficLightController.py
@@ -1,6 +1,13 @@
-
+from junction import Junction
+from light import Light
 
 class TrafficLightController():
 
     def __init__(self) -> None:          
         pass
+
+
+    def setup(self):
+        
+
+
