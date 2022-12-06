afd064e98727b14e37aea939f19b2d75c9b1abc5
darragh <120389806@umail.ucc.ie>
2022-12-06 14:02:18 +0000

    improved main to stop when ctrl c is pressed

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from setup import mainSetup
 from Vehicle import Vehicle
 from TrafficLightController import TrafficLightController
 import threading 
+import time
 
 class Main():
 
@@ -13,7 +14,12 @@ class Main():
         print(Junction.junctions)
         for junction in Junction.junctions:
             thread = threading.Thread(target=trafficLightController.controller, args=(junction,))
+            thread.daemon = True
             thread.start()
+
+        while True:
+            time.sleep(30)
+
         #create thread for generating vehicles
 
 
