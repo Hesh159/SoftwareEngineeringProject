24a75965e107429331b8ddb7a775f7c49d89b961
darragh <120389806@umail.ucc.ie>
2022-12-06 11:14:03 +0000

    started controller method in TrafficLightController

--- TrafficLightController.py
+++ TrafficLightController.py
@@ -4,11 +4,27 @@ from light import Light
 class TrafficLightController():
 
     def __init__(self) -> None:          
-        pass
+        self._baseTimePerCar = 2
 
 
-    def setup(self) -> None:
-        
-        pass
+    def controller(self, junctionToControl):
+        connectedLights = junctionToControl.getConnectedLightLists()
+        numberOfChangesPerCycle = len(connectedLights)   #change this to only change when pedestrian light
+                                                                                    #is pressed for junctions with only 2 neighbours
+        baseTimePerCar = 2
+
+        while True:
+            for connectedLightSet in connectedLights: #connectedLights is a dict and connectedLightSet is a key for the dict
+                                                        #the list of lights can be found in connectedLights[connectedLightSet]
+
+
+    def getCarsWaiting(self, lightList):
+        pass            
+
+
+
+
+
+
 
 
