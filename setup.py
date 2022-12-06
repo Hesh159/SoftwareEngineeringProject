2c0f754ae0d2fc236adc3478c71280ee93bed1e5
darragh <120389806@umail.ucc.ie>
2022-12-06 08:54:58 +0000

    setup.py - created createLights method

--- setup.py
+++ setup.py
@@ -21,6 +21,17 @@ class Setup():
         Junction4.addJunctionNeighbourPair(entryJunction2)
         self.createLights()
 
+    #automatically creates the systems traffic lights. For every junction it creates a light coming from a neighbouring junction, and going 
+    #to a neighbouring junction
     def createLights(self):
-        
+        for junction in Junction.getJunctions():
+            neighbouringJunctions = junction.getNeighbouringJunctions()
+            for prevJunction in neighbouringJunctions:
+                for destJunction in neighbouringJunctions:
+                    if prevJunction != destJunction:
+                        junction.addTrafficLight(prevJunction=prevJunction, destinationJunction=destJunction)
+            print(junction.getTrafficLights())
         return
+
+if __name__ == "__main__":
+    setupTest = Setup()
